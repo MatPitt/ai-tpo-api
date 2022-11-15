4f5b73520c3eea241c0710c3bc58e2cc2ef32a89
Nacho Rondan <nacho.rondan@gmail.com>
2022-11-15 11:48:30 -0300

    Agrego header para pasar tokenizado el user Id e integro creacion de perfil alumno

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import userController from '../../controllers/userController.js';
 import studentProfileController from '../../controllers/studentProfileController.js';
+import authorization from '../../auth/authorization.js';
 var route = express.Router();
 
 export default router => {
@@ -9,8 +10,13 @@ export default router => {
     route.get('/:id', (req, res) => userController.getUserById(req, res));
     route.post('/registration', (req, res) => userController.createUser(req, res));
     route.post('/login', (req, res) => userController.loginUser(req, res));
-    route.post('/student-registration', (req, res) => studentProfileController.createStudentProfile(
-            req, res, () => userController.updateUser(req, res))
-    );
+    route.post('/student-registration', (req, res) => authorization(
+        req,
+        res,
+        () => studentProfileController.createStudentProfile(
+            req,
+            res,
+            () => userController.updateUser(req, res))
+    ));
 
 };
