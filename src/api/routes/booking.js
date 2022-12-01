7188e0c3f32fe54a367c48850e6939f0fadd1f58
Nacho Rondan <nacho.rondan@gmail.com>
2022-12-01 08:46:09 -0300

    Agrego control de usuario logeado para contratar clases

--- src/api/routes/booking.js
+++ src/api/routes/booking.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bookingController from '../../controllers/bookingController.js';
+import authorization from "../../auth/authorization.js";
 var route = express.Router();
 
 export default router => {
     router.use('/bookings', route);
     route.get('/', (req, res) => bookingController.getAllBookings(req, res));
-    route.post('/', (req, res) => bookingController.createBooking(req, res));
+    route.post('/', (req, res) => authorization(req, res,
+        () => bookingController.createBooking(req, res)));
     route.delete('/:id', (req, res) => bookingController.deleteBookingById(req, res));
     route.post('/:id', (req, res) => bookingController.updateBooking(req, res));
 };
