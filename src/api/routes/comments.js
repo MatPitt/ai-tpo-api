d113ed427ecdf80d66f5edeb9a1fe7e8de7c00af
Nacho Rondan <nacho.rondan@gmail.com>
2022-12-06 11:43:14 -0300

    Agrego bloqueo y aceptado de comentarios

--- src/api/routes/comments.js
+++ src/api/routes/comments.js
@@ -9,5 +9,6 @@ export default router => {
     route.post('/', (req, res) => commentController.createComment(req, res));
     route.post('/:id', (req, res) => commentController.updateComment(req, res));
     route.get('/:id', (req, res) => commentController.getCommentsByCourseId(req, res));
+    route.get('/approved/:id', (req, res) => commentController.getCourseApprovedComments(req, res));
     route.delete('/:id/comments', (req, res) => commentController.deleteCommentById(req, res)); // No funciona
 };
